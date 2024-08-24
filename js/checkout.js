cfb167349e37ed5b35fb6fbc0c0d3e12a262bd33
mostafa20011999 <sasamostafa300@gmail.com>
2024-08-24 18:09:54 +0300

    promise all

--- js/checkout.js
+++ js/checkout.js
@@ -1,8 +1,11 @@
 import { rerunOrderSummary } from "./checkout/orderSummary.js";
 import {rerunPaymentSummary} from './checkout/paymentSummary.js'
 import { genProducts } from "../data/products.js";
-import '../data/backend.js';
-genProducts(()=>{
+Promise.all([
+  new Promise((resolve)=>{
+  genProducts(()=>{
+    resolve();
+  })})]).then(()=>{
   rerunOrderSummary();
   rerunPaymentSummary();
 })
